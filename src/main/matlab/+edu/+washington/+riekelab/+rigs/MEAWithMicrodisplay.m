c4dcd627a931209d79659c9d6ee885953937b1cf
Mike Manookin <manookin@uw.edu>
2022-04-25 10:49:01 -0700

    Update MEAWithMicrodisplay.m

--- src/main/matlab/+edu/+washington/+riekelab/+rigs/MEAWithMicrodisplay.m
+++ src/main/matlab/+edu/+washington/+riekelab/+rigs/MEAWithMicrodisplay.m
@@ -17,7 +17,7 @@ classdef MEAWithMicrodisplay < symphonyui.core.descriptions.RigDescription
             obj.addDevice(amp1);
             
             % Add the LEDs.
-            uvRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'suction', 'uv_led_gamma_ramp.txt'));
+            uvRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'uv_led_gamma_ramp.txt'));
             uv = CalibratedDevice('UV LED', Measurement.NORMALIZED, uvRamp(:, 1), uvRamp(:, 2)).bindStream(daq.getStream('ao1'));
             uv.addConfigurationSetting('ndfs', {}, ...
                 'type', PropertyType('cellstr', 'row', {'C1', 'C2', 'C3', 'C4', 'C5'}));
@@ -25,13 +25,13 @@ classdef MEAWithMicrodisplay < symphonyui.core.descriptions.RigDescription
                 {'C1', 'C2', 'C3', 'C4', 'C5'}, ...
                 {0.2768, 0.5076, 0.9281, 2.1275, 2.5022}));
             uv.addResource('fluxFactorPaths', containers.Map( ...
-                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'suction', 'uv_led_flux_factors.txt')}));
+                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'mea', 'uv_led_flux_factors.txt')}));
             uv.addConfigurationSetting('lightPath', '', ...
                 'type', PropertyType('char', 'row', {'', 'below', 'above'}));
-            uv.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'suction', 'uv_led_spectrum.txt')));          
+            uv.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'uv_led_spectrum.txt')));          
             obj.addDevice(uv);
             
-            blueRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'suction', 'blue_led_gamma_ramp.txt'));
+            blueRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'blue_led_gamma_ramp.txt'));
             blue = CalibratedDevice('Blue LED', Measurement.NORMALIZED, blueRamp(:, 1), blueRamp(:, 2)).bindStream(daq.getStream('ao2'));
             blue.addConfigurationSetting('ndfs', {}, ...
                 'type', PropertyType('cellstr', 'row', {'C1', 'C2', 'C3', 'C4', 'C5'}));
@@ -39,13 +39,13 @@ classdef MEAWithMicrodisplay < symphonyui.core.descriptions.RigDescription
                 {'C1', 'C2', 'C3', 'C4', 'C5'}, ...
                 {0.2663, 0.5389, 0.9569, 2.0810, 2.3747}));
             blue.addResource('fluxFactorPaths', containers.Map( ...
-                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'suction', 'blue_led_flux_factors.txt')}));
+                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'mea', 'blue_led_flux_factors.txt')}));
             blue.addConfigurationSetting('lightPath', '', ...
                 'type', PropertyType('char', 'row', {'', 'below', 'above'}));
-            blue.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'suction', 'blue_led_spectrum.txt')));                       
+            blue.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'blue_led_spectrum.txt')));                       
             obj.addDevice(blue);
             
-            greenRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_gamma_ramp.txt'));
+            greenRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'green_led_gamma_ramp.txt'));
             green = CalibratedDevice('Green LED', Measurement.NORMALIZED, greenRamp(:, 1), greenRamp(:, 2)).bindStream(daq.getStream('ao3'));
             green.addConfigurationSetting('ndfs', {}, ...
                 'type', PropertyType('cellstr', 'row', {'C1', 'C2', 'C3', 'C4', 'C5'}));
@@ -53,18 +53,18 @@ classdef MEAWithMicrodisplay < symphonyui.core.descriptions.RigDescription
                 {'C1', 'C2', 'C3', 'C4', 'C5'}, ...
                 {0.2866, 0.5933, 0.9675, 1.9279, 2.1372}));
             green.addResource('fluxFactorPaths', containers.Map( ...
-                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_flux_factors.txt')}));
+                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'mea', 'green_led_flux_factors.txt')}));
             green.addConfigurationSetting('lightPath', '', ...
                 'type', PropertyType('char', 'row', {'', 'below', 'above'}));
-            green.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_spectrum.txt')));            
+            green.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'green_led_spectrum.txt')));            
             obj.addDevice(green);
             
             % Add the Microdisplay
             ramps = containers.Map();
             ramps('minimum') = linspace(0, 65535, 256);
-            ramps('low')     = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_low_gamma_ramp.txt'));
-            ramps('medium')  = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_medium_gamma_ramp.txt'));
-            ramps('high')    = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_high_gamma_ramp.txt'));
+            ramps('low')     = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_low_gamma_ramp.txt'));
+            ramps('medium')  = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_medium_gamma_ramp.txt'));
+            ramps('high')    = 65535 * importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_high_gamma_ramp.txt'));
             ramps('maximum') = linspace(0, 65535, 256);
 %             microdisplay = riekelab.devices.MicrodisplayDevice('gammaRamps', ramps, 'micronsPerPixel', 3.0, 'comPort', 'COM4');
             microdisplay = riekelab.devices.MicrodisplayDevice('gammaRamps', ramps, 'micronsPerPixel', 2.88, 'comPort', 'COM4', 'host', '192.168.0.102');
@@ -88,16 +88,16 @@ classdef MEAWithMicrodisplay < symphonyui.core.descriptions.RigDescription
                     {0.26, 0.57, 0.93, 2.13, 0.29})}));
             microdisplay.addResource('fluxFactorPaths', containers.Map( ...
                 {'low', 'medium', 'high'}, { ...
-                riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_low_flux_factors.txt'), ...
-                riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_medium_flux_factors.txt'), ...
-                riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_high_flux_factors.txt')}));
+                riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_low_flux_factors.txt'), ...
+                riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_medium_flux_factors.txt'), ...
+                riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_high_flux_factors.txt')}));
             microdisplay.addConfigurationSetting('lightPath', 'below', 'isReadOnly', true);
             microdisplay.addResource('spectrum', containers.Map( ...
                 {'white', 'red', 'green', 'blue'}, { ...
-                importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_white_spectrum.txt')), ...
-                importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_red_spectrum.txt')), ...
-                importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_green_spectrum.txt')), ...
-                importdata(riekelab.Package.getCalibrationResource('rigs', 'confocal', 'microdisplay_below_blue_spectrum.txt'))}));
+                importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_white_spectrum.txt')), ...
+                importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_red_spectrum.txt')), ...
+                importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_green_spectrum.txt')), ...
+                importdata(riekelab.Package.getCalibrationResource('rigs', 'mea', 'microdisplay_below_blue_spectrum.txt'))}));
             obj.addDevice(microdisplay);
             
             % Add the frame monitor to record the timing of the monitor refresh.
