d3808f46e48431a543e49405b4bca56525488ad1
Mike Manookin <manookin@uw.edu>
2023-07-28 08:10:57 -0700

    Update TriggerDevice.m

--- src/main/matlab/+edu/+washington/+riekelab/+devices/TriggerDevice.m
+++ src/main/matlab/+edu/+washington/+riekelab/+devices/TriggerDevice.m
@@ -3,35 +3,10 @@ classdef TriggerDevice < symphonyui.core.Device
 
     methods
         
-        function obj = TriggerDevice(varargin)
-            ip = inputParser();
-            ip.addParameter('adc_type', 'analog', @ischar);
-            ip.parse(varargin{:});
-
-            if strcmpi(ip.Results.adc_type, 'analog')
-                adc_type = 'ANALOG';
-            else
-                adc_type = 'DIGITAL';
-            end
-            obj.adc_type = adc_type;
-            disp(adc_type)
-
-            if strcmp(adc_type, 'DIGITAL')
-                cobj = Symphony.Core.UnitConvertingExternalDevice('ExternalTrigger', 'none', Symphony.Core.Measurement(0, symphonyui.core.Measurement.UNITLESS));
-                
-                
-            else
-                cobj = Symphony.Core.UnitConvertingDevice('ExternalTrigger', 'V');
-            end
+        function obj = TriggerDevice()
+            cobj = Symphony.Core.UnitConvertingExternalDevice('ExternalTrigger', 'none', Symphony.Core.Measurement(0, symphonyui.core.Measurement.UNITLESS));
             obj@symphonyui.core.Device(cobj);
             obj.cobj.MeasurementConversionTarget = symphonyui.core.Measurement.UNITLESS;
-
-            % Add configuration settings.
-            obj.addConfigurationSetting('adc_type', adc_type);
-        end
-
-        function nm = get_adc_type(obj)
-            nm = obj.getConfigurationSetting('adc_type');
         end
 
         
